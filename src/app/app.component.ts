fbd1a9e4454cfccd728461bc34112de5b6978ec7
Danny <live626@gmail.com>
2023-12-26 13:46:40 -0400

    added security config

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
+import { ResourceService } from './services/resource.service';
 
 @Component({
   selector: 'app-root',
@@ -10,5 +11,34 @@ import { RouterOutlet } from '@angular/router';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+
   title = 'angular-gateway';
+  msguser = '';
+  msgadmin = '';
+
+  constructor(
+    private resourceService: ResourceService
+  ) {}
+
+  user(): void {
+    this.resourceService.user().subscribe({
+      next: (res: any) => (this.msguser = res.message),
+      error: (err: any) => (this.msguser = err.statusText + ': ' + err.status)
+    });
+  }
+
+  admin(): void {
+    this.resourceService.admin().subscribe({
+      next: (res: any) => (this.msgadmin = res.message),
+      error: (err: any) => (this.msgadmin = err.statusText + ': ' + err.status)
+    });
+  }
+
+  onLogin(): void {
+    window.location.href = '/oauth2/authorization/gateway';
+  }
+
+  onLogout() {
+    window.location.href = 'logout';
+  }
 }
